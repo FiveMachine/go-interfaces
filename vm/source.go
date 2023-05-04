21c4da7ed322ace1158c9a9b5c21c176ee2ae63c
FiveMachine <tlee07299@gmail.com>
2023-05-04 18:36:37 -0500

    3 resolver handles multi address (#4)
    * vm Backend requires a multi address, vm Resolver returns a multi address
    
    * add Timeouts for vm resolver and backend

--- vm/source.go
+++ vm/source.go
@@ -1,19 +1,23 @@
 package vm
 
-import "io"
+import (
+	"io"
+
+	ma "github.com/multiformats/go-multiaddr"
+)
 
 type Backend interface {
 	// Returns the URI scheme the backend supports.
 	Scheme() string
 	// Get attempts to retrieve the WASM asset.
-	Get(uri string) (io.ReadCloser, error)
+	Get(multAddr ma.Multiaddr) (io.ReadCloser, error)
 	// Close will close the Backend.
 	Close() error
 }
 
 type Resolver interface {
 	// Lookup resolves a module name and returns the uri
-	Lookup(ctx Context, module string) (string, error)
+	Lookup(ctx Context, module string) (ma.Multiaddr, error)
 }
 
 type Loader interface {
