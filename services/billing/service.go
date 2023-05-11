f436fe7831995d93fa69fd7e6a5bc0e82f27c1d8
FiveMachine <tlee07299@gmail.com>
2023-05-11 12:03:07 -0500

    added seer beacon

--- services/billing/service.go
+++ services/billing/service.go
@@ -1,14 +1,14 @@
 package billing
 
 import (
-	http "bitbucket.org/taubyte/http"
 	kv "bitbucket.org/taubyte/kvdb/database"
 	"github.com/taubyte/go-interfaces/services"
+	http "github.com/taubyte/http"
 )
 
 type Service interface {
 	services.Service
 	KV() *kv.KVDatabase
-	GitHubTokenHTTPAuth(ctx *http.Context) (interface{}, error)
-	GitHubTokenHTTPAuthCleanup(ctx *http.Context) (interface{}, error)
+	GitHubTokenHTTPAuth(ctx http.Context) (interface{}, error)
+	GitHubTokenHTTPAuthCleanup(ctx http.Context) (interface{}, error)
 }
