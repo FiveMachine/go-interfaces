756fdca63dd70e7cb5f083734d89031845744918
FiveMachine <tlee07299@gmail.com>
2023-09-14 09:55:02 -0500

    wip

--- services/monkey/client.go
+++ services/monkey/client.go
@@ -1,10 +1,15 @@
 package monkey
 
-import "github.com/taubyte/go-interfaces/services"
+import (
+	"github.com/ipfs/go-cid"
+	"github.com/taubyte/go-interfaces/services"
+	"github.com/taubyte/p2p/streams/command/response"
+)
 
 type Client interface {
 	services.Client
 	Status(jid string) (*StatusResponse, error)
 	Update(jid string, body map[string]interface{}) (string, error)
 	List() ([]string, error)
+	Cancel(cid cid.Cid, jid string) (response.Response, error)
 }
