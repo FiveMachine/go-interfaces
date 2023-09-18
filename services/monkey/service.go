6f8e986b9339f2363e8d8febace5cfe1d0984d25
FiveMachine <tlee07299@gmail.com>
2023-09-18 09:30:25 -0500

    added Hoarder Call to monkey interface (#34)

--- services/monkey/service.go
+++ services/monkey/service.go
@@ -2,13 +2,15 @@ package monkey
 
 import (
 	"github.com/taubyte/go-interfaces/services"
+	"github.com/taubyte/go-interfaces/services/hoarder"
 	"github.com/taubyte/go-interfaces/services/patrick"
 )
 
 type Service interface {
 	services.Service
-	Patrick() patrick.Client
 	Delete(jid string)
-
 	Dev() bool
+
+	Hoarder() hoarder.Client
+	Patrick() patrick.Client
 }
